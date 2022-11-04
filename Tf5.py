fd4971311be0810956dd640c8849ad76c99fcdee
s7rayn <adam.ruzicka.x@gmail.com>
2022-11-04 16:00:26 +0100

    update

--- Tf5.py
+++ Tf5.py
@@ -373,7 +373,8 @@ def show_frame():
                 # cv2.putText(frame1, labelt.title(), (135, 880), cv2.FONT_HERSHEY_PLAIN, 2, (34, 139, 34), 2)
                 if fr > 30:
                     cv2.putText(frame1, "S MASKOU", (230, 180), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
-                    cv2.putText(frame1, '{0:.2f}'.format(fr), (250, 210), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2,
+                    cv2.putText(frame1, "TEPLOTA OK", (230, 280), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+                    cv2.putText(frame1, '{0:.2f}'.format(fr), (250, 360), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2,
                                 cv2.LINE_AA)
                     m_t_k = cv2.resize(m_t_k, (frame1.shape[1], frame1.shape[0]))
                     dst = cv2.addWeighted(frame1, 0.5, m_t_k, 0.2, 0)
@@ -383,7 +384,8 @@ def show_frame():
                 # labelt = "Abnormal Temperature"
                 # cv2.putText(frame1, labelt.title(), (130, 180), cv2.FONT_HERSHEY_PLAIN, 2, (0,0,255), 2)
                 cv2.putText(frame1, "S MASKOU", (230, 180), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
-                cv2.putText(frame1, '{0:.2f}'.format(fr), (250, 210), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2,
+                cv2.putText(frame1, "VYSOKA TEPLOTA", (230, 280), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+                cv2.putText(frame1, '{0:.2f}'.format(fr), (250, 360), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2,
                             cv2.LINE_AA)
                 m_k_t_n = cv2.resize(m_k_t_n, (frame1.shape[1], frame1.shape[0]))
                 dst = cv2.addWeighted(frame1, 0.5, m_k_t_n, 0.2, 0)
@@ -404,8 +406,9 @@ def show_frame():
                 # labelt = "Temeparature Normal"
                 # cv2.putText(frame1, labelt.title(), (135, 336), cv2.FONT_HERSHEY_PLAIN, 2, (34, 139, 34), 2)
                 if fr > 30:
-                    cv2.putText(frame1, "BEZ MASKY\nTEPLOTA OK", (230, 180), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
-                    cv2.putText(frame1, '{0:.2f}'.format(fr), (250, 366), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2,
+                    cv2.putText(frame1, "BEZ MASKY", (230, 180), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+                    cv2.putText(frame1, "TEPLOTA OK", (230, 280), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
+                    cv2.putText(frame1, '{0:.2f}'.format(fr), (250, 360), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2,
                                 cv2.LINE_AA)
                     m_n_t_k = cv2.resize(m_n_t_k, (frame1.shape[1], frame1.shape[0]))
                     dst = cv2.addWeighted(frame1, 0.5, m_n_t_k, 0.2, 0)
@@ -414,7 +417,8 @@ def show_frame():
                 # 130, 880
                 # cv2.putText(frame1, labelt.title(), (130, 180), cv2.FONT_HERSHEY_PLAIN, 2, (0,0,255), 2)
                 cv2.putText(frame1, "BEZ MASKY", (230, 180), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
-                cv2.putText(frame1, '{0:.2f}'.format(fr), (250, 250), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2,
+                cv2.putText(frame1, "VYSOKA TEPLOTA", (230, 280), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+                cv2.putText(frame1, '{0:.2f}'.format(fr), (250, 360), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2,
                             cv2.LINE_AA)
                 m_n_t_n = cv2.resize(m_n_t_n, (frame1.shape[1], frame1.shape[0]))
                 dst = cv2.addWeighted(frame1, 0.5, m_n_t_n, 0.2, 0)
