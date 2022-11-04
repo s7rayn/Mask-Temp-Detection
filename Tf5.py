6fdbbae20c8e04d79fc61bd9f21bc997c57ca878
s7rayn <adam.ruzicka.x@gmail.com>
2022-11-04 16:01:38 +0100

    update

--- Tf5.py
+++ Tf5.py
@@ -385,8 +385,7 @@ def show_frame():
                 # cv2.putText(frame1, labelt.title(), (130, 180), cv2.FONT_HERSHEY_PLAIN, 2, (0,0,255), 2)
                 cv2.putText(frame1, "S MASKOU", (230, 180), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0), 2)
                 cv2.putText(frame1, "VYSOKA TEPLOTA", (230, 280), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
-                cv2.putText(frame1, '{0:.2f}'.format(fr), (250, 360), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2,
-                            cv2.LINE_AA)
+                cv2.putText(frame1, '{0:.2f}'.format(fr), (250, 360), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2, cv2.LINE_AA)
                 m_k_t_n = cv2.resize(m_k_t_n, (frame1.shape[1], frame1.shape[0]))
                 dst = cv2.addWeighted(frame1, 0.5, m_k_t_n, 0.2, 0)
                 # dst = cv2.addWeighted(frame1, 0.5, dst, 0.2, 0)
